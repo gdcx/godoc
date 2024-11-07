760e141ce155579aa7e38c9b7312d353d3c8f313
wxJoyce <291777143@qq.com>
2024-11-07 17:42:27 +0800

    update video_demo and doc

--- video_demo/webpack.config.js
+++ video_demo/webpack.config.js
@@ -1,12 +1,17 @@
 const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: { index: path.resolve(__dirname, "src", "index.js") },
     plugins: [
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, "src", "index.html")
-        })
+        }),
+        new CopyWebpackPlugin([
+            {from: path.resolve(__dirname, 'public'),to: 'public'}
+        ])
+      
     ],
     module: {
         rules: [
